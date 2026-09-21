b8423daae7a8a760d4b43fcf6a5629bfb18835a8
realfshaw-crypto <realfshaw@users.noreply.github.com>
2026-09-22 02:07:41 +0800

    刷机后内置 opkg 源改为教育网镜像 mirrors.cernet.edu.cn

--- firmware_build/diy.sh
+++ firmware_build/diy.sh
@@ -11,3 +11,14 @@ echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki' >> feeds.co
 
 # 主题：Proton2025（当前正在用的主题，clone 进 package 编译）
 git clone --depth 1 https://github.com/ChesterGoodiny/luci-theme-proton2025 package/luci-theme-proton2025
+
+# 刷机后内置 opkg 软件源改为教育网镜像 mirrors.cernet.edu.cn
+# 首次启动时自动替换 /etc/opkg/distfeeds.conf 的官方源
+cat > package/base-files/files/etc/uci-defaults/99-custom-distfeeds << 'EOF'
+#!/bin/sh
+# 将 opkg 官方源替换为国内教育网镜像
+sed -i 's#https://downloads.immortalwrt.org#https://mirrors.cernet.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+sed -i 's#https://mirrors.vsean.net/openwrt#https://mirrors.cernet.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+exit 0
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/99-custom-distfeeds
